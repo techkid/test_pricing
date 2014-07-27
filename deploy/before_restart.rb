cfb8b1d19a53048c5623561fd15dddbfb4a504eb
László Györke <laslo.derke@gmail.com>
2014-07-28 00:43:25 +0200

    fix the deploy hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,5 @@
 Chef::Log.info("Running deploy/before_restart.rb...")
 
-execute "symlink MWS config" do
-  link "#{release_path}/config/mws.yml" do
-    to "#{new_resource.deploy_to}/shared/config/database.yml"
-  end
+link "#{release_path}/config/mws.yml" do
+  to "#{new_resource.deploy_to}/shared/config/database.yml"
 end
