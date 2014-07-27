6ecf2393e4d843c7083243d11107daf32dfb9e11
László Györke <laslo.derke@gmail.com>
2014-07-28 01:57:38 +0200

    debug-1

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,3 +1,5 @@
 Chef::Log.info("Running deploy/before_restart.rb...")
 
-FileUtils.cp "#{new_resource.deploy_to}/shared/config/mws.rb", "#{release_path}/config/mws.rb"
+Chef::Log.info(new_resource.inspect)
+
+#FileUtils.cp "#{new_resource.deploy_to}/shared/config/mws.rb", "#{release_path}/config/mws.rb"
