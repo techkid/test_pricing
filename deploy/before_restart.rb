c8f3e30b411b9db03a29cd3f02bd87dc64cc41a7
László Györke <laslo.derke@gmail.com>
2014-07-28 02:23:15 +0200

    fixed user/group reference

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -3,7 +3,7 @@ Chef::Log.info("Running deploy/before_restart.rb...")
 template "#{release_path}/config/mws.rb" do
   source "#{release_path}/deploy/templates/mws.rb.erb"
   mode "0660"
-  owner deploy[:user]
-  group deploy[:group]
+  owner new_resource.user
+  group new_resource.group
   variables(:data => (node[:deploy][new_resource.application.to_sym][:mws] || {}), :environment => new_resource.environment["RAILS_ENV"])
 end.run_action(:create)
