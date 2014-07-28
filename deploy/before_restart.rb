5e26e38ba418406e26014e91c6d7c52ddea8c775
László Györke <laslo.derke@gmail.com>
2014-07-28 02:16:24 +0200

    add config template

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,9 @@
 Chef::Log.info("Running deploy/before_restart.rb...")
 
-Chef::Log.info(new_resource.inspect)
-
-#FileUtils.cp "#{new_resource.deploy_to}/shared/config/mws.rb", "#{release_path}/config/mws.rb"
+template "#{release_path}/config/mws.rb" do
+  source "#{release_path}/deploy/templates/mws.rb.erb"
+  mode "0660"
+  owner deploy[:user]
+  group deploy[:group]
+  variables(:data => (node[:deploy][new_resource.application.to_sym][:mws] || {}), :environment => new_resource.environment["RAILS_ENV"])
+end.run_action(:create)
